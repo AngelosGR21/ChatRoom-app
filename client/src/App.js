830bec6b47195ca5107aa5c23906abb51e694397
AngelosGR21 <angelgrigoriu@gmail.com>
2022-06-01 10:24:47 +0100

    created socket connection with server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import Lobby from "./components/Lobby/Lobby";
+import { io } from "socket.io-client";
 
 function App() {
+  const [id, setId] = useState(0);
+  const [socket, setSocket] = useState("");
+
+  useEffect(() => {
+    const socket = io("http://localhost:5000");
+    setSocket(socket);
+    socket.on("connect", () => {
+      "connected! ~ socket";
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Lobby socket={socket} />
+      <p>{id !== 0 ? id : "No id"}</p>
+    </div>
   );
 }
 
